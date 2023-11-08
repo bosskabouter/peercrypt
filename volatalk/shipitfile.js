dd29f0fc8401669f3faf8d40de14a01b478d65e5
bosskabouter <bosskabouter@protonmail.com>
2023-11-08 22:35:47 +0000

    ci

--- volatalk/shipitfile.js
+++ volatalk/shipitfile.js
@@ -1,67 +1,65 @@
 module.exports = (shipit) => {
-  require("shipit-deploy")(shipit);
-  require("shipit-shared")(shipit);
-  require("shipit-pm2")(shipit);
+  require('shipit-deploy')(shipit);
+  require('shipit-shared')(shipit);
+  require('shipit-pm2')(shipit);
 
-  const projectRoot = "./volatalk";
+  const projectRoot = './volatalk';
 
   shipit.initConfig({
     default: {
-      servers: "admin@volatalk.org",
-      branch: "master",
-      deployTo: projectRoot + "/test",
-      repositoryUrl: "https://github.com/bosskabouter/peercrypt.git",
+      servers: 'admin@volatalk.org',
+      branch: 'master',
+      deployTo: projectRoot + '/test',
+      repositoryUrl: 'https://github.com/bosskabouter/peercrypt.git',
       keepReleases: 5,
-      shared: {
-        overwrite: true,
-        dirs: ["node_modules"],
-      },
+      // shared: {
+      //   overwrite: true,
+      //   dirs: ['node_modules'],
+      // },
     },
     test: {
-      branch: "develop",
-      deployTo: projectRoot + "/test",
+      branch: 'develop',
+      deployTo: projectRoot + '/test',
       pm2: {
-        json: projectRoot + "/test/current/server/pm2.test.config.js",
-
+        json: projectRoot + '/test/current/server/pm2.test.config.js',
       },
     },
     prod: {
-      branch: "master",
-      deployTo: projectRoot + "/prod",
+      branch: 'master',
+      deployTo: projectRoot + '/prod',
       pm2: {
-        json: projectRoot + "/server/pm2.prod.config.js",
-
+        json: projectRoot + '/server/pm2.prod.config.js',
       },
     },
   });
 
-  shipit.on("published", async function () {
+  shipit.on('published', async function () {
     const conf = shipit.config;
-
 
     const srcClientBuild = `${conf.deployTo}/current/client/build/*`;
     const desWWW = `/var/www/volatalk/${conf.branch}/`;
     const cmdCopyClientWWW = `mkdir ${desWWW} -p & cp -R ${srcClientBuild} ${desWWW}`;
 
     const cdServer = `cd ${conf.deployTo}/current/ && `;
-    await runRemote(cdServer + `npm i && npx nx run volatalk-server:build:production`);
-                                               
+    await runRemote(
+      cdServer + `npm i && nx run-many --targets=build`
+    ).catch(console.error);
 
-   // runRemote(cmdCopyClientWWW);
+    // runRemote(cmdCopyClientWWW);
   });
 
   async function runRemote(cmd) {
-    shipit.log("CMD: " + cmd);
+    shipit.log('CMD: ' + cmd);
     let result = (await shipit.remote(cmd))[0].child;
-   
-    shipit.log("exitCode: " + result.exitCode);
+
+    shipit.log('exitCode: ' + result.exitCode);
   }
 
-  const path = require("path");
+  const path = require('path');
   const ecosystemFilePath = path.join(
     shipit.config.deployTo,
-    "shared",
-    "ecosystem.config.js"
+    'shared',
+    'ecosystem.config.js'
   );
 
   // Our listeners and tasks will go here
